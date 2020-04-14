01d4107979ddb49238067560557c2f72ffdad699
Taoheed King <tking92@montgomerycollege.edu>
2020-04-14 14:52:34 -0400

    Add files via upload
    Doctors office practice

---
+++ JavaSecondProject/src/doctorsOffice.java
@@ -0,0 +1,17 @@
+public class doctorsOffice {
+        public static void main(String[] args) {
+
+            String patientName = "Stacey Manson";
+            boolean hasInsurance = false;
+            int temp = 102;
+            double numVisits = 5.02;
+
+            System.out.println("Patient Details:");
+            System.out.println("Name: "+patientName);
+            System.out.println("Patient has insurance: "+hasInsurance);
+            System.out.println("Body temperature: " + temp);
+            System.out.println("Number of visit: " + numVisits);
+
+
+        }
+}
